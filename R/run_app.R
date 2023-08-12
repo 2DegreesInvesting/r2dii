34ec2179512547051621f2354a1f8b330da30f42
Mauro Lepore <maurolepore@gmail.com>
2023-08-12 19:31:33 +0000

    Paginate

--- R/run_app.R
+++ R/run_app.R
@@ -10,40 +10,40 @@ run_app <- function() {
 
   options(readr.show_col_types = FALSE)
 
-  ui <- fluidPage(
-    h1("Match"),
-    h2("Inputs"),
-    fileInput("loanbook", "Bank's data", accept = ".csv"),
-    DTOutput("loanbook"),
-    fileInput("companies", "Delivered data", accept = ".csv"),
-    DTOutput("companies"),
-    h2("Output"),
-    strong("Matched"),
-    DTOutput("matched"),
-    downloadButton("to_validate", "Save in '~/Downloads/to_validate.csv'"),
-
-    h1("Apply methodology"),
-    h2("Inputs"),
-    fileInput("validated", "Validated data", accept = ".csv"),
-    DTOutput("validated"),
-    fileInput("scenarios", "Additional data (scenario)", accept = ".csv"),
-    DTOutput("scenarios"),
-    fileInput("regions", "Additional data (regions)", accept = ".csv"),
-    DTOutput("regions"),
-    h2("Output"),
-    strong("Table"),
-    DTOutput("market_share"),
-    downloadButton("result", "Save in '~/Downloads/result.csv'"),
-    br(),
-    br(),
-    p(strong("Plot")),
-    selectInput(
-      "sector",
-      "Sector",
-      choices = c("automotive", "oil and gas", "power")
+  ui <- navbarPage("r2dii",
+    tabPanel("Match",
+      h2("Inputs"),
+      fileInput("loanbook", "Bank's data", accept = ".csv"),
+      DTOutput("loanbook"),
+      fileInput("companies", "Delivered data", accept = ".csv"),
+      DTOutput("companies"),
+      h2("Output"),
+      strong("Matched"),
+      DTOutput("matched"),
+      downloadButton("to_validate", "Save in '~/Downloads/to_validate.csv'")
     ),
-    plotOutput("sector")
-
+    tabPanel("Apply methodology",
+      h2("Inputs"),
+      fileInput("validated", "Validated data", accept = ".csv"),
+      DTOutput("validated"),
+      fileInput("scenarios", "Additional data (scenario)", accept = ".csv"),
+      DTOutput("scenarios"),
+      fileInput("regions", "Additional data (regions)", accept = ".csv"),
+      DTOutput("regions"),
+      h2("Output"),
+      strong("Table"),
+      DTOutput("market_share"),
+      downloadButton("result", "Save in '~/Downloads/result.csv'"),
+      br(),
+      br(),
+      p(strong("Plot")),
+      selectInput(
+        "sector",
+        "Sector",
+        choices = c("automotive", "oil and gas", "power")
+      ),
+      plotOutput("sector")
+    )
   )
 
   server <- function(input, output, session) {
@@ -51,45 +51,67 @@ run_app <- function() {
       req(input$loanbook)
       read_csv(input$loanbook$datapath)
     })
-    output$loanbook <- renderDT({loanbook()})
+    output$loanbook <- renderDT({
+      loanbook()
+    })
     companies <- reactive({
       req(input$companies)
       read_csv(input$companies$datapath)
     })
-    output$companies <- renderDT({companies()})
+    output$companies <- renderDT({
+      companies()
+    })
     matched <- reactive({
       req(input$companies)
       match_name(loanbook(), companies())
     })
-    output$matched <- renderDT({matched()})
+    output$matched <- renderDT({
+      matched()
+    })
     output$to_validate <- downloadHandler(
-      filename = function() {"to_validate.csv"},
-      content = function(file) {write_csv(matched(), file)}
+      filename = function() {
+        "to_validate.csv"
+      },
+      content = function(file) {
+        write_csv(matched(), file)
+      }
     )
 
     validated <- reactive({
       req(input$validated)
       prioritize(read_csv(input$validated$datapath))
     })
-    output$validated <- renderDT({validated()})
+    output$validated <- renderDT({
+      validated()
+    })
     scenarios <- reactive({
       req(input$scenarios)
       read_csv(input$scenarios$datapath)
     })
-    output$scenarios <- renderDT({scenarios()})
+    output$scenarios <- renderDT({
+      scenarios()
+    })
     regions <- reactive({
       req(input$regions)
       read_csv(input$regions$datapath)
     })
-    output$regions <- renderDT({regions()})
+    output$regions <- renderDT({
+      regions()
+    })
     market_share <- reactive({
       req(input$regions)
       target_market_share(validated(), companies(), scenarios(), regions())
     })
-    output$market_share <- renderDT({market_share()})
+    output$market_share <- renderDT({
+      market_share()
+    })
     output$result <- downloadHandler(
-      filename = function() {"result.csv"},
-      content = function(file) {write_csv(market_share(), file)}
+      filename = function() {
+        "result.csv"
+      },
+      content = function(file) {
+        write_csv(market_share(), file)
+      }
     )
 
     output$sector <- renderPlot({
